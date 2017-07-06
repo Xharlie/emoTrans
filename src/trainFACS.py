ef4a6521c8419101db9f1441fcaceae076cd831b
xharlie <charlie.xu007@yahoo.com>
2017-07-06 22:56:47 +0800

    preTrainEmbeddingSpace

--- src/trainFACS.py
+++ src/trainFACS.py
@@ -12,22 +12,45 @@ import importlib
 import itertools
 import argparse
 import utility
+from tensorflow.python.ops import data_flow_ops
 
 HOME = os.path.expanduser("~")
-model_def = 'models.inception_resnet_v1'
-logs_base_dir='~/dev/emoTrans/'
-seed = 666
-data_dir = HOME + "/datasets/BosphorusDB"
+
+args = {
+    'model_def' : 'models.inception_resnet_v1',
+    'logs_base_dir' : '~/dev/emoTrans/',
+    'seed': 666,
+    'data_dir' : HOME + "/datasets/BosphorusDB/train",
+    'val_dir' : HOME + "/datasets/BosphorusDB/val",
+    'random_crop' : True,
+    'image_size' : 160,
+    'batch_size' : 90,
+    'keep_probability' : 1.0,
+    'embedding_size' : 200,
+    'weight_decay' : 0.1,
+    'learning_rate_decay_epochs' : 1.0,
+    'epoch_size' : 1000,
+    'learning_rate_decay_factor' : 1.0,
+    'optimizer' : 'ADAGRAD',
+    'moving_average_decay' : 0.9999,
+    'gpu_memory_fraction' : 1.0,
+    'pretrained_model' : None,
+    'max_nrof_epochs' : 500,
+    'people_per_batch' : None,
+    'images_per_person' : None,
+    'validation_batch_num' : 3
+}
+
 
 def main():
-    network = importlib.import_module(model_def, 'inference')
+    network = importlib.import_module(args.model_def, 'inference')
     subdir = datetime.strftime(datetime.now(), '%Y%m%d-%H%M%S')
-    log_dir = os.path.join(os.path.expanduser(logs_base_dir), subdir)
-    np.random.seed(seed=seed)
-    train_set = utility.get_dataset(data_dir)
+    log_dir = os.path.join(os.path.expanduser(args.logs_base_dir), subdir)
+    np.random.seed(seed=args.seed)
+    train_set = utility.get_dataset(args.data_dir)
 
     with tf.Graph().as_default():
-        tf.set_random_seed(seed)
+        tf.set_random_seed(args.seed)
         global_step = tf.Variable(0, trainable=False)
 
         # Placeholder for the learning rate
@@ -38,18 +61,226 @@ def main():
 
         phase_train_placeholder = tf.placeholder(tf.bool, name='phase_train')
 
-        image_paths_placeholder = tf.placeholder(tf.string, shape=(None, 3), name='image_paths')
+        image_paths_placeholder = tf.placeholder(tf.string, shape=(None, 2), name='image_paths')
 
-        labels_placeholder = tf.placeholder(tf.int64, shape=(None, 3), name='labels')
+        labels_placeholder = tf.placeholder(tf.int64, name='labels')
 
         input_queue = data_flow_ops.FIFOQueue(capacity=100000,
                                               dtypes=[tf.string, tf.int64],
-                                              shapes=[(3,), (3,)],
+                                              shapes=[(2,), (2,)],
                                               shared_name=None, name=None)
         enqueue_op = input_queue.enqueue_many([image_paths_placeholder, labels_placeholder])
 
         nrof_preprocess_threads = 4
-        images_and_labels = []
+        images_pair_and_labels = []
+
+        for _ in range(nrof_preprocess_threads):
+            filename_pairs, label = input_queue.dequeue()
+            image_pairs = []
+            for pair in tf.unstack(filename_pairs):
+                processed_pair = []
+                for i in range(2):
+                    file_contents = tf.read_file(pair[i])
+                    image = tf.image.decode_png(file_contents)
+
+                    if args.random_crop:
+                        image = tf.random_crop(image, [args.image_size, args.image_size, 3])
+                    else:
+                        image = tf.image.resize_image_with_crop_or_pad(image, args.image_size, args.image_size)
+                    #     WE MAY NOT WANT TO FLIP
+                    # if args.random_flip:
+                    #     image = tf.image.random_flip_left_right(image)
+
+                    # pylint: disable=no-member
+                    image.set_shape((args.image_size, args.image_size, 3))
+                    processed_pair.append(tf.image.per_image_standardization(image))
+                image_pairs.append(processed_pair)
+            images_pair_and_labels.append([image_pairs[0],image_pairs[1], label])
+
+        source_batch, target_batch, labels_batch = tf.train.batch_join(
+            images_pair_and_labels, batch_size=batch_size_placeholder,
+            shapes=[(args.image_size, args.image_size, 3),
+                    (args.image_size, args.image_size, 3), ()], enqueue_many=True,
+            capacity=4 * nrof_preprocess_threads * args.batch_size,
+            allow_smaller_final_batch=True)
+        prelogits, _ = network.inference(source_batch, target_batch, args.keep_probability,
+            phase_train = phase_train_placeholder, bottleneck_layer_size = args.embedding_size,
+            weight_decay = args.weight_decay)
+
+        embeddings = tf.nn.l2_normalize(prelogits, 1, 1e-10, name='embeddings')
+        # sourceIMG, targetIMG = tf.unstack(tf.reshape(embeddings, [-1,2, args.embedding_size]), 2, 1)
+        # FACS of source and target
+        # sourceLabels, targetLabels = tf.unstack(tf.reshape(labels_batch, [-1,2, args.embedding_size]), 2, 1)
+        trans_loss = utility.trans_loss(embeddings, labels_batch)
+
+        learning_rate = tf.train.exponential_decay(learning_rate_placeholder, global_step,
+                                                   args.learning_rate_decay_epochs * args.epoch_size,
+                                                   args.learning_rate_decay_factor, staircase=True)
+        tf.summary.scalar('learning_rate', learning_rate)
+
+        # Calculate the total losses
+        regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        total_loss = tf.add_n([trans_loss] + regularization_losses, name='total_loss')
+
+        # Build a Graph that trains the model with one batch of examples and updates the model parameters
+        train_op = utility.train(total_loss, global_step, args.optimizer,
+                                 learning_rate, args.moving_average_decay, tf.global_variables())
+
+        # Create a saver
+        saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=3)
+
+        # Build the summary operation based on the TF collection of Summaries.
+        summary_op = tf.summary.merge_all()
+
+        # Start running operations on the Graph.
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction= args.gpu_memory_fraction)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+        # Initialize variables
+        sess.run(tf.global_variables_initializer(), feed_dict={phase_train_placeholder: True})
+        sess.run(tf.local_variables_initializer(), feed_dict={phase_train_placeholder: True})
+
+        summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
+        coord = tf.train.Coordinator()
+        tf.train.start_queue_runners(coord=coord, sess=sess)
+
+        with sess.as_default():
+
+            if args.pretrained_model:
+                print('Restoring pretrained model: %s' % args.pretrained_model)
+                saver.restore(sess, os.path.expanduser(args.pretrained_model))
+
+            # Training and validation loop
+            epoch = 0
+            while epoch < args.max_nrof_epochs:
+                step = sess.run(global_step, feed_dict=None)
+                epoch = step // args.epoch_size
+                # Train for one epoch
+                train(args, sess, train_set, epoch, image_paths_placeholder,
+                      labels_placeholder, labels_batch,batch_size_placeholder,
+                      learning_rate_placeholder, phase_train_placeholder, enqueue_op,
+                      input_queue, global_step,embeddings, total_loss, train_op,
+                      summary_op, summary_writer, args.learning_rate_schedule_file,
+                      args.embedding_size, trans_loss)
+
+                # Save variables and the metagraph if it doesn't exist already
+                save_variables_and_metagraph(sess, saver,
+                     summary_writer, args.model_dir, subdir, step)
+
+                # Evaluate on itself
+                if args.val_dir:
+                    evaluate(sess, enqueue_op, image_paths_placeholder, labels_placeholder,labels_batch,
+                             phase_train_placeholder, batch_size_placeholder, embeddings, trans_loss,
+                             labels_batch, args.val_dir, args.lfw_batch_size, log_dir,
+                             step, summary_writer)
+    sess.close()
+
+
+def train(args, sess, dataset, epoch, image_paths_placeholder, labels_placeholder,
+          labels_batch, batch_size_placeholder, learning_rate_placeholder,
+          phase_train_placeholder, enqueue_op, input_queue, global_step,
+          embeddings, loss, train_op, summary_op, summary_writer,
+          learning_rate_schedule_file, embedding_size, trans_loss):
+    batch_number = 0
+    batch_size = args.batch_size
+    if args.learning_rate > 0.0:
+        lr = args.learning_rate
+    else:
+        lr = utility.get_learning_rate_from_file(learning_rate_schedule_file, epoch)
+    while batch_number < args.epoch_size:
+        # Sample people randomly from the dataset
+        image_paths_pair, label_trans_array = \
+            utility.sample_pair(dataset, args.batch_size, args.people_per_batch);
+
+        print('Running forward pass on sampled images: ', end='')
+        start_time = time.time()
+
+        sess.run(enqueue_op, {image_paths_placeholder: image_paths_pair
+            , labels_placeholder: label_trans_array})
+        start_time = time.time()
+        feed_dict = {batch_size_placeholder: batch_size, learning_rate_placeholder: lr,
+                     phase_train_placeholder: True}
+        err, _, step, emb, lab = sess.run([loss, train_op, global_step, embeddings, labels_batch],
+                                          feed_dict=feed_dict)
+        duration = time.time() - start_time
+        print('Epoch: [%d][%d/%d]\tTime %.3f\tLoss %2.3f' %
+              (epoch, batch_number + 1, args.epoch_size, duration, err))
+        batch_number += 1
+
+        # Add validation loss and accuracy to summary
+        summary = tf.Summary()
+        # pylint: disable=maybe-no-member
+        # summary.value.add(tag='time/selection', simple_value=selection_time)
+        summary_writer.add_summary(summary, step)
+    return step
+
+
+def save_variables_and_metagraph(sess, saver, summary_writer, model_dir, model_name, step):
+    # Save the model checkpoint
+    print('Saving variables')
+    start_time = time.time()
+    checkpoint_path = os.path.join(model_dir, 'model-%s.ckpt' % model_name)
+    saver.save(sess, checkpoint_path, global_step=step, write_meta_graph=False)
+    save_time_variables = time.time() - start_time
+    print('Variables saved in %.2f seconds' % save_time_variables)
+    metagraph_filename = os.path.join(model_dir, 'model-%s.meta' % model_name)
+    save_time_metagraph = 0
+    if not os.path.exists(metagraph_filename):
+        print('Saving metagraph')
+        start_time = time.time()
+        saver.export_meta_graph(metagraph_filename)
+        save_time_metagraph = time.time() - start_time
+        print('Metagraph saved in %.2f seconds' % save_time_metagraph)
+    summary = tf.Summary()
+    # pylint: disable=maybe-no-member
+    summary.value.add(tag='time/save_variables', simple_value=save_time_variables)
+    summary.value.add(tag='time/save_metagraph', simple_value=save_time_metagraph)
+    summary_writer.add_summary(summary, step)
+
+  # evaluate(sess, enqueue_op, image_paths_placeholder, labels_placeholder,
+  #                            phase_train_placeholder, batch_size_placeholder, embeddings,
+  #                            labels_batch, args.val_dir, args.lfw_batch_size, log_dir,
+  #                            step, summary_writer)
+def evaluate(sess, enqueue_op, image_paths_placeholder, labels_placeholder, labels_batch,
+             phase_train_placeholder, batch_size_placeholder, embeddings, trans_loss,
+             labels, image_paths, batch_size, nrof_folds, log_dir, step, summary_writer):
+    start_time = time.time()
+    # Run forward pass to calculate embeddings
+    print('Runnning forward pass on validation images')
+
+    batch_number = 0
+    batch_size = args.batch_size
+    dataset = utility.get_dataset(image_paths)
+    sumloss = 0
+    while batch_number < args.validation_batch_num:
+        # Sample people randomly from the dataset
+        image_paths_pair, label_trans_array = \
+            utility.sample_pair(dataset, batch_size, args.people_per_batch);
+
+        sess.run(enqueue_op, {image_paths_placeholder: image_paths_pair
+            , labels_placeholder: label_trans_array})
+        start_time = time.time()
+        feed_dict = {batch_size_placeholder: batch_size, phase_train_placeholder: False}
+        err, emb, lab = sess.run([trans_loss, embeddings, labels_batch],
+                                          feed_dict=feed_dict)
+        sumloss += err
+        duration = time.time() - start_time
+        batch_number += 1
+    avg_loss = sumloss / batch_number
+
+
+    print('Validation loss: %1.3f+-%1.3f' % avg_loss)
+    val_time = time.time() - start_time
+    # Add validation loss and accuracy to summary
+    summary = tf.Summary()
+    # pylint: disable=maybe-no-member
+    summary.value.add(tag='val/loss', simple_value=avg_loss)
+    summary_writer.add_summary(summary, step)
+
+    with open(os.path.join(log_dir, 'val_result.txt'), 'at') as f:
+        f.write('%d\t%.5f\t\n' % (step, avg_loss))
+
+
 
 if __name__ == '__main__':
     main()
--- src/utility.py
+++ src/utility.py
@@ -1,5 +1,11 @@
 
 import os
+import numpy as np
+import random
+import re
+import tensorflow as tf
+
+HOME = os.path.expanduser("~")
 
 class ImageClass():
     "Stores the paths to images for a given class"
@@ -30,3 +36,179 @@ def get_dataset(paths):
                 dataset.append(ImageClass(class_name, image_paths))
 
     return dataset
+
+
+def sample_pair(dataset, batch_size, people_per_batch):
+    nrof_classes = len(dataset)
+    if people_per_batch is None or nrof_classes < people_per_batch:
+        people_per_batch = min(nrof_classes, batch_size)
+
+    # Sample classes from the dataset
+    class_indices = np.arange(nrof_classes)
+    np.random.shuffle(class_indices)
+    avg_pair_per_people = batch_size / people_per_batch
+    i = 0
+    image_pair_paths = []
+    label_calculated_result = []
+    codes_array = load_facscodes()
+    # Sample images from these classes until we have enough
+    while len(image_pair_paths) < batch_size:
+        class_index = class_indices[i]
+        nrof_images_in_class = len(dataset[class_index])
+        pair_per_people = min(batch_size - len(image_pair_paths), avg_pair_per_people)
+        for j in range(pair_per_people):
+            indice1 = random.randint(0, nrof_images_in_class - 1)
+            indice2 = random.randint(0, nrof_images_in_class - 1)
+            image_paths_for_class = [dataset[class_index].image_paths[indice1],
+                                     dataset[class_index].image_paths[indice2]]
+            label_calculated_result += calculate_labels(codes_array,
+                dataset[class_index].image_paths[indice1], dataset[class_index].image_paths[indice2])
+            image_pair_paths += image_paths_for_class
+        i += 1
+    return
+
+def load_facscodes():
+    codes_array = {}
+    with open(HOME + '/dev/emoTrans/facscodes.lst') as fp:
+        for line in fp:
+            pair = line.split("->")
+            codes_array[pair[0].strip()] = pair[1].strip()
+    return codes_array
+
+def calculate_labels(codes_array, image_path1, image_path2):
+    calculate_codes_1 = embedding_translate(image_path1)
+    calculate_codes_2 = embedding_translate(image_path2)
+    return calculate_codes_2 - calculate_codes_1
+
+def embedding_translate(image_path):
+    codes = np.zeros(shape = (200))
+    dimensionList = image_path.split("+")
+    for code_expression in dimensionList:
+        code = int(re.findall('\d+', code_expression)[0])
+        alphabet = code_expression.split(str(code))
+        pos = 0
+        strength = 0
+        if len(alphabet) == 1:
+            strength = alphabet[0]
+        elif alphabet[0] == 'L':
+            pos = 1
+            strength = alphabet[1]
+        elif alphabet[0] == 'R':
+            pos = 2
+            strength = alphabet[1]
+        else:
+            strength = alphabet[1]
+        strength_float = {
+            'A': 0.2,
+            'B': 0.4,
+            'C': 0.6,
+            'D': 0.8,
+            'E': 1.0
+        }.get(strength)
+        if pos == 0:
+            codes[code - 1] = strength_float
+            codes[code + 100] = strength_float
+        elif pos == 1:
+            codes[code] = strength_float
+        elif pos == 2:
+            codes[code + 100] = strength_float
+        else:
+            print "Wrong!"
+    print codes
+
+
+def get_learning_rate_from_file(filename, epoch):
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            line = line.split('#', 1)[0]
+            if line:
+                par = line.strip().split(':')
+                e = int(par[0])
+                lr = float(par[1])
+                if e <= epoch:
+                    learning_rate = lr
+                else:
+                    return learning_rate
+
+def trans_loss(embeddings, labels_batch):
+    #Calculate the trans loss
+    with tf.variable_scope('trans_loss'):
+        dist = tf.reduce_sum(tf.square(tf.subtract(embeddings, labels_batch)), 1)
+    return dist
+
+
+def train(total_loss, global_step, optimizer, learning_rate, moving_average_decay, update_gradient_vars,
+          log_histograms=True):
+    # Generate moving averages of all losses and associated summaries.
+    loss_averages_op = _add_loss_summaries(total_loss)
+
+    # Compute gradients.
+    with tf.control_dependencies([loss_averages_op]):
+        if optimizer == 'ADAGRAD':
+            opt = tf.train.AdagradOptimizer(learning_rate)
+        elif optimizer == 'ADADELTA':
+            opt = tf.train.AdadeltaOptimizer(learning_rate, rho=0.9, epsilon=1e-6)
+        elif optimizer == 'ADAM':
+            opt = tf.train.AdamOptimizer(learning_rate, beta1=0.9, beta2=0.999, epsilon=0.1)
+        elif optimizer == 'RMSPROP':
+            opt = tf.train.RMSPropOptimizer(learning_rate, decay=0.9, momentum=0.9, epsilon=1.0)
+        elif optimizer == 'MOM':
+            opt = tf.train.MomentumOptimizer(learning_rate, 0.9, use_nesterov=True)
+        else:
+            raise ValueError('Invalid optimization algorithm')
+
+        grads = opt.compute_gradients(total_loss, update_gradient_vars)
+
+    # Apply gradients.
+    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+    # Add histograms for trainable variables.
+    if log_histograms:
+        for var in tf.trainable_variables():
+            tf.summary.histogram(var.op.name, var)
+
+    # Add histograms for gradients.
+    if log_histograms:
+        for grad, var in grads:
+            if grad is not None:
+                tf.summary.histogram(var.op.name + '/gradients', grad)
+
+    # Track the moving averages of all trainable variables.
+    variable_averages = tf.train.ExponentialMovingAverage(
+        moving_average_decay, global_step)
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
+
+    with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
+        train_op = tf.no_op(name='train')
+
+    return train_op
+
+
+def _add_loss_summaries(total_loss):
+    """Add summaries for losses.
+
+    Generates moving average for all losses and associated summaries for
+    visualizing the performance of the network.
+
+    Args:
+      total_loss: Total loss from loss().
+    Returns:
+      loss_averages_op: op for generating moving averages of losses.
+    """
+    # Compute the moving average of all individual losses and the total loss.
+    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+    losses = tf.get_collection('losses')
+    loss_averages_op = loss_averages.apply(losses + [total_loss])
+
+    # Attach a scalar summmary to all individual losses and the total loss; do the
+    # same for the averaged version of the losses.
+    for l in losses + [total_loss]:
+        # Name each loss as '(raw)' and name the moving average version of the loss
+        # as the original loss name.
+        tf.summary.scalar(l.op.name + ' (raw)', l)
+        tf.summary.scalar(l.op.name, loss_averages.average(l))
+
+    return loss_averages_op
+
+if __name__ == '__main__':
+    load_facscodes()
